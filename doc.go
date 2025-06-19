782b82b90e8f5ab59235d30db5e8eb13731dfaa0
jonoton <65074561+jonoton@users.noreply.github.com>
2025-06-19 07:52:40 -0400

    update doc

--- doc.go
+++ doc.go
@@ -1,51 +1,75 @@
 /*
 # scout
+
 Scout is a standalone open source software solution for DIY video security.
 
 https://www.jonoton-innovation.com
 
-## Features
-*No monthly fees!*
-*Easy Installation*
-* Lives on your device, on your network
-* Build from source or use Docker pre-built image
-* Customize configuration with YAML
-* Link multiple servers together
-*Hassle-free management*
-* Need to adjust the camera? No problem! No reconfigure required!
-* No database!
-* Stores images and recordings to your local file system
-* You own your data
-* Auto-prune old data
-*Computer Vision*
-* Extends already proven libraries for Motion, Object, and Face detection
-* Helps reduce False Positive alerts due to issues such as spider webs or the sun peeking from behind clouds
-*Alerts*
-* Get notified via email or text when an alert occurs
-*Recordings*
-* Automatically record when objects are detected
-*Continuous*
-* Continuous recording with auto-prune
+# Features
 
-## Mobile Client Features
-*Access your Scout server remotely*
-* Home and Away settings
-*Live view of monitors*
-* Quick view of all monitors
-* Live Fullscreen with Pan and Zoom
-*Alerts*
-* Scroll through alerts
-* Alerts Fullscreen with Pan and Zoom
-* Share or Download alerts
-* Platform Notifications for Object, Person, and Face detection
-*Recordings*
-* Scroll through recordings
-* Recordings Fullscreen with Progressive loading
-* Share or Download recordings
-*Continuous*
-* Scroll through continuous recordings
-* Continuous recordings Fullscreen with Progressive loading
-* Share or Download continuous recordings
+No monthly fees!
+
+Easy Installation
+
+  - Lives on your device, on your network
+  - Build from source or use Docker pre-built image
+  - Customize configuration with YAML
+  - Link multiple servers together
+
+Hassle-free management
+
+  - Need to adjust the camera? No problem! No reconfigure required!
+  - No database!
+  - Stores images and recordings to your local file system
+  - You own your data
+  - Auto-prune old data
+
+Computer Vision
+
+  - Extends already proven libraries for Motion, Object, and Face detection
+  - Helps reduce False Positive alerts due to issues such as spider webs or the sun peeking from behind clouds
+
+Alerts
+
+  - Get notified via email or text when an alert occurs
+
+Recordings
+
+  - Automatically record when objects are detected
+
+Continuous
+
+  - Continuous recording with auto-prune
+
+# Mobile Client Features
+
+Access your Scout server remotely
+
+  - Home and Away settings
+
+Live view of monitors
+
+  - Quick view of all monitors
+  - Live Fullscreen with Pan and Zoom
+
+Alerts
+
+  - Scroll through alerts
+  - Alerts Fullscreen with Pan and Zoom
+  - Share or Download alerts
+  - Platform Notifications for Object, Person, and Face detection
+
+Recordings
+
+  - Scroll through recordings
+  - Recordings Fullscreen with Progressive loading
+  - Share or Download recordings
+
+Continuous
+
+  - Scroll through continuous recordings
+  - Continuous recordings Fullscreen with Progressive loading
+  - Share or Download continuous recordings
 
 Android
 View App in Google Play Store https://play.google.com/store/apps/details?id=com.jonoton.scout
@@ -54,11 +78,15 @@ View App in Amazon Appstore https://www.amazon.com/gp/mas/dl/android?p=com.jonot
 iOS
 View App in iOS Store https://apps.apple.com/us/app/scout-full/id1658406270
 
-## Web Client Limited Features
-*Verify setup visually*
-* RAM usage
-* Frame Rate displayed
-*Live view of monitors*
-* Quick view of all monitors
+# Web Client Limited Features
+
+Verify setup visually
+
+  - RAM usage
+  - Frame Rate displayed
+
+Live view of monitors
+
+  - Quick view of all monitors
 */
 package main
